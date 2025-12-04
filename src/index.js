5878a16bc316f5d84c9c368eb82c61e9567653fd
julepdesign <julepgraphicdesign@gmail.com>
2025-12-04 13:19:20 +0100

    Injected weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
-}
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#speed");
+  let dayElement = document.querySelector("#day");
+  let timeElement = document.querySelector("#time");
+  let yearElement = document.querySelector("#year");
+  let date = new Date(response.data.time * 1000);
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let year = date.getFullYear();
 
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  dayElement.innerHTML = weekDays[date.getDay()];
+  timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+  yearElement.innerHTML = year;
+}
 function searchCity(city) {
   let apiKey = "c5a77b10o404035t8533ef4bf02afcfc";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
